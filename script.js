56713da8b684bdcb419c530d9187c2ef2363a7d5
Cauã Arthur <cauaarthurdossantos@gmail.com>
2024-11-19 21:56:27 -0300

    design aaaaaaaaa

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ var formulariologin = document.getElementById('formlogin')
 var botaologin = document.getElementById('loginbutton')
 var formulariocadastro = document.getElementById('formcad')
 var botaocadastro = document.getElementById('cadbutton')
+var login = document.getElementById('pralogar')
+var cadastro = document.getElementById('pracadastrar')
 
 function logar() {
     formulariologin.style.display = 'flex'
     botaologin.style.display = 'none'
     formulariocadastro.style.display = 'none'
     botaocadastro.style.display = 'flex'
+    login.style.backgroundColor = 'white'
+    cadastro.style.backgroundColor = 'rgba(255, 32, 32, 0.925)'
 }
 
 function cadastrar() {
@@ -15,4 +19,7 @@ function cadastrar() {
     botaologin.style.display = 'flex'
     formulariocadastro.style.display = 'flex'
     botaocadastro.style.display = 'none'
+    login.style.backgroundColor = 'rgba(255, 32, 32, 0.925)'
+    cadastro.style.backgroundColor = 'white'
+
 }
